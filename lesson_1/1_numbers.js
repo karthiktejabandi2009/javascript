12414c01ccd8c00c986f78f48445cbdbddc3b04f
Mahesh Bandi <mbandi1985@gmail.com>
2020-08-23 21:22:19 +0530

    Lesson one

---
+++ lesson_1/1_numbers.js
@@ -0,0 +1,11 @@
+console.log(2+3); //5
+console.log(2*5); //10
+console.log(2-5); //-3
+console.log(10/5); //2
+console.log(5%5);   // 0
+console.log(2**3); //2*2*2 = 8
+console.log(4**5); //4*4*4*4*4 = 1024
+console.log(1/0); //Infinity
+console.log(0/1); //0
+console.log(0/0); //NaN
+console.log(2+3+(3*5));//20
